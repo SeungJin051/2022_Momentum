c6b86cacb10c47ef556cb3eabe4de5e8434174a6
SeungJin051 <seungjin051@gmail.com>
2022-10-14 01:04:10 +0900

    add deleting todo (object)
    printToDo를 문자가아닌 객체로 받게끔 수정 (객체의 속성값은 줌)

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 const TODOS_KEY = "todos";
 
-let toDos = []; // save toDos // todos는 항상 빈 array로 시작하여서 이전의 todos가 사라짐, let array를 parse해준다.
+let toDos = []; // save toDos
 
 function saveToDos() {
   localStorage.setItem("TODOS_KEY", JSON.stringify(toDos)); // JSON.stringify = string으로 변환 ex)JSON.stringify([1, 2, 3, 4])
@@ -17,23 +17,29 @@ function deleteToDo(event) {
 
 function printToDo(createToDo) {
   const li = document.createElement("li");
+  li.id = createToDo.id; // li에 객체의 속성값을 전달
   const span = document.createElement("span");
   const button = document.createElement("button");
 
-  span.innerHTML = createToDo;
+  span.innerHTML = createToDo.text; // span에 객체의 속성값을 전달
   button.innerHTML = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-
   toDoList.appendChild(li);
 }
 
-function handleToDoSubmit() {
+function handleToDoSubmit(event) {
+  event.preventDefault();
   const createToDo = toDoInput.value; // input의 현재 value를 새로운 변수에 복사
   toDoInput.value = ""; // 초기화
-  toDos.push(createToDo);
-  printToDo(createToDo); // newTodo(파라미터)값을 printToDo 함수로 보냄
+  const newToDosObj = {
+    // toDos에 문자가 아닌 객체를 저장
+    text: createToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDosObj);
+  printToDo(newToDosObj); // newTodo(파라미터)값을 printToDo 함수로 보냄
   saveToDos();
 }
 
@@ -44,8 +50,8 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 } */
 
 const savedToDos = localStorage.getItem("TODOS_KEY");
+
 if (saveToDos !== null) {
-  const parsedToDos = JSON.parse(savedToDos);
-  toDos = parsedToDos; // todos는 항상 빈 array로 시작하여서 이전의 todos가 사라짐, let array를 parse해준다. (복원, 예전값을 저장과 새로운 값을 저장)
-  parsedToDos.forEach(printToDo); // forEach
+  const parseToDos = JSON.parse(savedToDos);
+  parseToDos.forEach(printToDo); // todos는 항상 빈 array로 시작하여서 이전의 todo가 사라짐, let array를 parse해준다. // ex) printToDo({text: 'a', id: 1235346})
 }
