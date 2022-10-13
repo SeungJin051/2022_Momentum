7caf30cb2df91b69a5acc869be8d8e7d636dfb24
SeungJin051 <seungjin051@gmail.com>
2022-10-13 15:32:58 +0900

    add todo

--- js/todo.js
+++ js/todo.js
@@ -1,2 +1,16 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input");
+
+function printToDo(newTodo) {
+  console.log("i will print ToDo", newTodo);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const createTodo = toDoInput.value; // input의 현 value를 변수에 복사.
+  toDoInput.value = ""; // 초기화
+  printToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", printToDo);
