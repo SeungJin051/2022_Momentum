1721c0852c97fd779be172131904f9e495a66532
SeungJin051 <seungjin051@gmail.com>
2022-10-13 23:01:05 +0900

    add delete todo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+// click(event)
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function printToDo(createToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
+  const button = document.createElement("button");
+
   span.innerHTML = createToDo;
+  button.innerHTML = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+
   toDoList.appendChild(li);
 }
 
